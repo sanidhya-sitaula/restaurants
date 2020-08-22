f7c029c4679a82f60c698b9b0467f5b90c1ade96
sanidhya-sitaula <sanidhya.sitaula1999@gmail.com>
2020-08-21 21:11:17 -0500

    Updates

---
+++ precache-manifest.a3226c4f44747ea76734bdaf20c1b37c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93523eb7abbcd61f1581b3d8226ffd1",
+    "url": "/restaurants/index.html"
+  },
+  {
+    "revision": "58d491466183c8e2f8f5",
+    "url": "/restaurants/static/css/main.3e9bce16.chunk.css"
+  },
+  {
+    "revision": "826637b2f491518518c8",
+    "url": "/restaurants/static/js/2.1e2b1a78.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/restaurants/static/js/2.1e2b1a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d491466183c8e2f8f5",
+    "url": "/restaurants/static/js/main.ecdf0fea.chunk.js"
+  },
+  {
+    "revision": "3faf666bb07a5b91f48a",
+    "url": "/restaurants/static/js/runtime-main.41f6af45.js"
+  }
+]);
